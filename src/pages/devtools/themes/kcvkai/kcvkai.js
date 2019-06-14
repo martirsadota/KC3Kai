bf109270ac9e9c87cfdf47fd851067103bf67815
martirsadota <cln.nagisa@gmail.com>
2019-06-14 20:18:19 +0800

    placeholders

---
+++ src/pages/devtools/themes/kcvkai/kcvkai.js
@@ -0,0 +1,32 @@
+// placeholder
+
+$(document).on('ready', function(){
+    // initialization code
+    // tab switcher
+    $('.tabs-tab').on('click', function() {
+        let target = $(this).data('target');
+        $('.tabs-tab').removeClass('active');
+        $(this).addClass('active');
+        $('.panel-content').hide();
+        $(`#content-${target}`).show();
+    });
+
+    // set UI to ready state
+    makeReady();
+    // hide the overlay
+    $('#waiting-overlay').hide();
+});
+
+function makeReady() {
+    $('#statusbar').removeClass('waiting onsortie').addClass('ready');
+    $('#tab-panel').removeClass('onsortie');
+    $('#status-text').html('Ready');
+    $('#tab-main').click();
+}
+
+function onSortie(map) {
+    $('#statusbar').removeClass('ready').addClass('onsortie');
+    $('#tab-panel').addClass('onsortie');
+    $('#status-text').html(`On sortie: map ${map}`);
+    $('#tab-battle').click();
+}
